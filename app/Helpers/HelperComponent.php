65f7c15e1ac93e5be6c9d099cd7007f76bbf9f4c
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2021-11-15 23:22:55 +0500

    update sidebar menu

--- app/Helpers/HelperComponent.php
+++ app/Helpers/HelperComponent.php
@@ -48,43 +48,207 @@ class HelperComponent
                 ],
 
             ],
-            [
-                "single_link" => setSingleLink("Customers", "users", "customers-view", "/customers"),
-            ],
+         
             [
                 "heading" => "Apps",
             ],
-            [
+            // [
+
+            //     "menu" => [
+            //         "title" => 'Ecommerce',
+            //         "icon" => "shopping-cart",
+            //         "sub_menu" => [
+            //             setSubMenu(
+            //                 "Categories",
+            //                 null,
+            //                 "categories-view",
+            //                 "/categories",
+            //             ),
+            //             setSubMenu(
+            //                 "Products",
+            //                 null,
+            //                 "products-view",
+            //                 "/products",
+            //             ),
+
+            //             setSubMenu(
+            //                 "Orders",
+            //                 null,
+            //                 "orders-view",
+            //                 "/orders",
+            //             ),
+
+            //         ]
+
+            //     ],
+
+            // ],
+             [
 
                 "menu" => [
-                    "title" => 'Ecommerce',
+                    "title" => 'Orders',
                     "icon" => "shopping-cart",
                     "sub_menu" => [
                         setSubMenu(
-                            "Categories",
+                            "New Orders",
                             null,
-                            "categories-view",
-                            "/categories",
+                            "new-order-view",
+                            "/new-order",
                         ),
                         setSubMenu(
-                            "Products",
+                            "Accepted Orders",
                             null,
-                            "products-view",
-                            "/products",
+                            "accepted-orders-view",
+                            "/accepted-orders",
                         ),
 
                         setSubMenu(
-                            "Orders",
+                            "Pending Orders",
                             null,
-                            "orders-view",
-                            "/orders",
+                            "pending-orders-view",
+                            "/pending-orders",
+                        ),
+                        setSubMenu(
+                            "Pre Orders",
+                            null,
+                            "pre-orders-view",
+                            "/pre-orders",
+                        ),
+                        setSubMenu(
+                            "Declined Orders",
+                            null,
+                            "declined-orders-view",
+                            "/declined-orders",
+                        ),
+                       setSubMenu(
+                            "Delivered Orders",
+                            null,
+                            "delivered-orders-view",
+                            "/Delivered-orders",
                         ),
 
                     ]
 
                 ],
 
-            ],
+              ],
+              [
+
+                "menu" => [
+                    "title" => 'Members',
+                    "icon" => "users",
+                    "sub_menu" => [
+                        setSubMenu(
+                            "New Members",
+                            null,
+                            "new-members-view",
+                            "/new-members",
+                        ),
+                        setSubMenu(
+                            "Messages",
+                            null,
+                            "Messages-view",
+                            "/member/messages",
+                        ),
+
+                        setSubMenu(
+                            "Verifications Members",
+                            null,
+                            "verifications-members-view",
+                            "/members/verifications",
+                        ),
+                        setSubMenu(
+                            "Gold Members",
+                            null,
+                            "gold-members-view",
+                            "/gold-members",
+                        ),
+                       setSubMenu(
+                            "Queries & Alerts",
+                            null,
+                            "queries-members-view",
+                            "/queries-members",
+                        ),
+
+                    ]
+
+                ],
+
+              ],
+             [
+
+                "menu" => [
+                    "title" => 'Customers',
+                    "icon" => "users",
+                    "sub_menu" => [
+                        setSubMenu(
+                            "New Customers",
+                            null,
+                            "new-customers-view",
+                            "/new-customers",
+                        ),
+                        setSubMenu(
+                            "Messages",
+                            null,
+                            "customers-messages-view",
+                            "/customers/messages",
+                        ),
+
+                        setSubMenu(
+                            "Complaints",
+                            null,
+                            "complaints-customers-view",
+                            "/customers/complaints",
+                        ),
+                      
+                    ]
+
+                ],
+
+              ],
+             [
+
+                "menu" => [
+                    "title" => 'Subscribers',
+                    "icon" => "credit-card",
+                    "sub_menu" => [
+                        setSubMenu(
+                            "New Subscribers",
+                            null,
+                            "new-customers-view",
+                            "/new-customers",
+                        ),
+                        setSubMenu(
+                            "Current Subscribers",
+                            null,
+                            "customers-messages-view",
+                            "/customers/messages",
+                        ),
+
+                        setSubMenu(
+                            "Recurring Payments",
+                            null,
+                            "complaints-customers-view",
+                            "/customers/complaints",
+                        ),
+                        setSubMenu(
+                            "Cancelled Subscription",
+                            null,
+                            "complaints-customers-view",
+                            "/customers/complaints",
+                        ),
+                        setSubMenu(
+                            "Complaints",
+                            null,
+                            "complaints-customers-view",
+                            "/customers/complaints",
+                        ),
+                      
+                    ]
+
+                ],
+
+              ],
             [
                 "heading" => "Content",
             ],
